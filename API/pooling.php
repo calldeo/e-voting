60f02213253e38eb0f655f8bb7307fe90a46aaf5
adityaibrar <adityaibrar@gmail.com>
2022-12-05 15:55:52 +0700

    fixed login and add pooling API

--- API/pooling.php
+++ API/pooling.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 include('connection.php');
 
-$query = "SELECT * FROM calon_osis";
+$query = "SELECT nama_calon, SUM(jumlah_vote) as hasil FROM calon_osis GROUP BY nama_calon";
 $response = array();
 $result = mysqli_query($koneksi, $query);
 while ($row = mysqli_fetch_assoc($result)) {
