1ca6853c5b8510d03f9f5f3738e908af169731b4
adityaibrar <adityaibrar@gmail.com>
2023-01-10 20:33:31 +0700

    use count percentage pooling

--- API/pooling.php
+++ API/pooling.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 include('connection.php');
 
-$query = "SELECT calon_osis.nama_calon ,COUNT(id_user) as jumlah_vote FROM hasil_voting INNER JOIN calon_osis ON calon_osis.id_calon = hasil_voting.id_calon GROUP BY hasil_voting.id_calon";
+$query = "SELECT calon_osis.nama_calon, ROUND(COUNT(calon_osis.id_calon)/(SELECT COUNT(hasil_voting.id_user) FROM hasil_voting)*100.0) AS jumlah_vote FROM hasil_voting INNER JOIN calon_osis ON calon_osis.id_calon = hasil_voting.id_calon GROUP BY hasil_voting.id_calon";
 $response = array();
 $result = mysqli_query($koneksi, $query);
 while ($row = mysqli_fetch_assoc($result)) {
